7fd44be1a4939d88bcfbd72d21a1a5d4b8f38759
LeeJones95 <leonna.jones18@gmail.com>
2021-06-28 18:34:11 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return "This one is on me!"
+  }
+   else if (someValue > 2000) {
+     return "I will gladly take your thirty bucks."
+  } 
+  else if (someValue > 2500) {
+    return "No can do."
+  }
+// Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return (cityName = 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipGenerosity){
+  switch (tipGenerosity) {
+    case 'generous' : return "Thank you so much."
+    case 'not as generous' : return "Thank you."
+    case 'thanks for everything' :return "Bye."
+  }
+   
 }
